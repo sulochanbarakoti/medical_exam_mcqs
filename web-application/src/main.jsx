84a77ab623e137747f32d64a20d3d3a57a7c3b58
sulochan barakoti <sulochanbarakoti@gmail.com>
2024-08-30 12:54:59 +0300

    created web application

---
+++ web-application/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root.jsx";
+
+const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);

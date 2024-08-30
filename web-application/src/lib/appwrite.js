313258e2cd6ab41a349e5243a506db20948230e0
sulochan barakoti <sulochanbarakoti@gmail.com>
2024-08-30 18:45:58 +0300

    some update on appwrite

--- web-application/src/lib/appwrite.js
+++ web-application/src/lib/appwrite.js
@@ -1,10 +1,4 @@
-import { Client, Databases, ID, Query } from "appwrite";
-// import physicsQuestions from "../../questions/physics";
-// import chemistryQuestions from "../questions/chemistry";
-// import botanyQuestions from "../questions/botany";
-// import zoologyQuestions from "../questions/zoology";
-
-// const questions = physicsQuestions;
+import { Client, Databases } from "appwrite";
 
 export const config = {
   databaseId: "66bb3704002e68cbd0ca",
@@ -17,8 +11,8 @@ export const config = {
 // Initialize Appwrite Client
 const client = new Client();
 client
-  .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
-  .setProject("66bb36c60038c891f881"); // Your project ID
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("66bb36c60038c891f881");
 
 // Initialize Database SDK
 const databases = new Databases(client);
@@ -37,43 +31,3 @@ export async function fetchQuestions(collectionId) {
     console.error("Error fetching questions:", error);
   }
 }
-
-// Call the function with the appropriate collection ID (for example, 'Physics')
-// fetchQuestions("[COLLECTION_ID]");
-
-// async function insertQuestion(collectionId, questions) {
-//   for (const que of questions) {
-//     try {
-//       // Check if the question already exists in the collection
-//       const existingQuestions = await databases.listDocuments(
-//         config.databaseId,
-//         collectionId,
-//         [Query.equal("question", que.question)]
-//       );
-
-//       if (existingQuestions.total > 0) {
-//         console.log("Skipping duplicate question:", que.question);
-//         continue; // Skip this question if it already exists
-//       }
-
-//       const response = await databases.createDocument(
-//         config.databaseId,
-//         collectionId,
-//         ID.unique(),
-//         {
-//           question: que.question,
-//           options: que.options,
-//           correctAnswer: que.correctAnswer,
-//           solution: que.solution,
-//         }
-//       );
-//       console.log("Question inserted:", response);
-//     } catch (error) {
-//       console.error("Error inserting question:", error);
-//     }
-//   }
-// }
-
-// export function update() {
-//   insertQuestion(config.physicsCollectionId, questions);
-// }
